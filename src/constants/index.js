8bdcfd42119794f488e6bfe6a13260e46228d538
Hans Andrade <74209424+hardraco@users.noreply.github.com>
2026-03-09 22:09:16 -0400

    Delete src directory

--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const Status = {
-    ACTIVE: 'ACTIVO',
-    INACTIVE: 'INACTIVO'
-}
--- src/validators/user.validate.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const createSchema = joi.object({
-    username: Joi.string().alphanum().min(3).max(38).required(),
-
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-});
